614503ab38cef0fc1c9055f0da8cc498e3a1514b
equidevium <equidevium@users.noreply.github.com>
2026-03-17 12:52:08 +0100

    small warning about the height of HomePage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function HomePage() {
   return (
     <section className="flex flex-1 flex-col">
       <div className="flex flex-1 items-center justify-center">
-        <div className="h-[600px] w-full max-w-4xl">
+        <div className="h-150 w-full max-w-4xl">
           <EarthSceneLoader />
         </div>
       </div>
